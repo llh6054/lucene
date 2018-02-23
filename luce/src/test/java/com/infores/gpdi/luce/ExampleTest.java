25f17c9b54e05b36f3e41a336c534663b582a5ab
chubby <605403092@qq.com>
2018-02-23 16:13:56 +0800

    test

--- luce/src/test/java/com/infores/gpdi/luce/ExampleTest.java
+++ luce/src/test/java/com/infores/gpdi/luce/ExampleTest.java
@@ -42,9 +42,9 @@ public class ExampleTest {
 	
 	@Test
 	public void standardTokenizerImpTest() throws IOException {
-		String s = "I'm LaiLongHui, my email is a605403092@gmail.com. My ip address is 192.168.0.1, AT&T and I.B.M are all great companies.";
+	//	String s = "I'm LaiLongHui, my email is a605403092@gmail.com. My ip address is 192.168.0.1, AT&T and I.B.M are all great companies.";
 	//	String s = "我是赖龙辉";
-	//	String s = "わたしは 頼です";
+		String s = "わたしは 頼です";
 		StringReader reader = new StringReader(s);
 
 		StandardTokenizerImpl impl = new StandardTokenizerImpl(reader);
